707557b4e46186285ecf7785022f1a2f6970c82f
miguelrodfher <miguelrodfher@gmail.com>
2020-05-17 10:30:42 -0400

    studio work

---
+++ studio/Rocket.ts
@@ -0,0 +1,56 @@
+import { Payload } from './Payload';
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[]): number {
+        let sum: number = 0;
+        for (let item of items) {
+            sum += item.massKg;
+        }
+        return sum;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo) === true) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut) === true) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
